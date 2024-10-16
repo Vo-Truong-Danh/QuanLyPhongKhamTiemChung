8d59ce9337d0ef25c27231f0a3ff664059511849
DanhDayNe <vtd1501@gmail.com>
2024-10-16 07:51:11 +0700

    xoa debug

--- QuanLyPhongKham/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ QuanLyPhongKham/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf2a147356bacd93f1a9775ace1331678cd9123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52e2c385122cab5bac6bcd6f152101267c7884a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
